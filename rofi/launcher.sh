a123eed2815c521d5c1a7fb68fe37be60952c43c
Ale <999hipukema999@protonmail.com>
2022-06-16 13:12:40 +0200

    Rofi solo necesarios

---
+++ rofi/launcher.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+theme="lanzador"
+dir="$HOME/.config/rofi/"
+
+rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
